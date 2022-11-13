56b4115e61fa1156bf57d88bad666c744d13003d
Mark Carlton <mcarlton33@gmail.com>
2022-11-13 19:44:28 +0300

    include prisma

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,15 @@ import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
 import { logger } from './logger/logger.middleware';
+import { PrismaService } from './services/prisma/prisma.service';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.use(logger)
+
+  const prismaService = app.get(PrismaService);
+  await prismaService.enableShutdownHooks(app)
+
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
